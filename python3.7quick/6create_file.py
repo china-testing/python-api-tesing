b022ad1f127f6a0dd609ffdfcd12bde738c4e42c
china-testing <x_r_zh@163.com>
2018-11-10 16:10:55 +0800

    Add create_file.py

---
+++ python3.7quick/6create_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 讨论钉钉免费群21745728 qq群144081101 567351477
+# CreateDate: 2018-11-10
+
+f= open("china-testing.github.io.txt","w+")
+for i in range(10):
+     f.write("This is line %d\r\n" % (i+1))
+f.close() 
