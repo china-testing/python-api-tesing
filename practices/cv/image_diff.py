6599d6912b6bb1e329e8999e305aaf77951c4eb4
china-testing <x_r_zh@163.com>
2018-12-23 00:05:00 +0800

    Add practices/cv/image_diff.py

---
+++ practices/cv/image_diff.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 项目实战讨论QQ群630011153 144081101
+# https://github.com/china-testing/python-api-tesing/blob/master/practices/cv/image_diff.py
+# 代码及配套图片：https://itbooks.pipipan.com/fs/18113597-326879233
+# CreateDate: 2018-12-21
+# USAGE
+# python image_diff.py --first images/original_01.png --second images/modified_01.png
+
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--first", required=True,
+	help="first input image")
+ap.add_argument("-s", "--second", required=True,
+	help="second")
+args = vars(ap.parse_args())
+
+# load the two input images
+imageA = cv2.imread(args["first"])
+imageB = cv2.imread(args["second"])
+
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# loop over the contours
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
