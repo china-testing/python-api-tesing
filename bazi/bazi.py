0067ce123eabeaaf6bb879b02c23764433444dcb
china-testing <x_r_zh@163.com>
2019-02-28 17:26:05 +0800

    modify bazi.py format 正官 to 官

--- bazi/bazi.py
+++ bazi/bazi.py
@@ -13,7 +13,6 @@ from bidict import bidict
 
 from datas import *
 
-
 description = '''
 
 '''
@@ -27,6 +26,7 @@ parser.add_argument('time', action="store", help=u'time')
 parser.add_argument('-b', action="store_true", default=False, help=u'直接输入八字')
 parser.add_argument('-g', action="store_true", default=False, help=u'是否采用公历')
 parser.add_argument('-r', action="store_true", default=False, help=u'是否为闰月，仅仅使用于农历')
+parser.add_argument('-n', action="store_true", default=False, help=u'是否为女，默认为男')
 parser.add_argument('--version', action='version',
                     version='%(prog)s 0.1 Rongzhong xu 2019 02 21')
 options = parser.parse_args()
@@ -70,30 +70,31 @@ if not options.b:
     print("\t{}年{}{}月{}日".format(day.Lyear0 + 1984, Lleap, ymc[day.Lmc], rmc[day.Ldi]))
 
 print("\n八字:   同义词：七杀|偏官 偏印|枭神 阳刃|帝旺(阳干)|冠带(阴干)")
-print("="*140)    
-print("{:<30s}{:<30s}{:<30s}{:<30s}".format('年【父-根】', "月【兄弟僚友-苗】", "日【自己配偶-花】", "时【子孙-实】"))
-print("-"*140)
-print("{:>11s}-{:<19s}{:>11s}-{:<19s}{:>11s}-{:<19s}{:>11s}-{:<19s}".format(
-    gans.year, '{} [{}]'.format(gan5[gans.year], ten_deities[me][gans.year]),
-    gans.month, '{} [{}]'.format(gan5[gans.month], ten_deities[me][gans.month]),
-    me, '{} [{}]'.format(gan5[me], '自己'), 
-    gans.time, '{} [{}]'.format(gan5[gans.time], ten_deities[me][gans.time]),
+print("="*150)    
+print("{:^30s}{:^30s}{:^30s}{:^30s}".format('年【父-根】', "月【兄弟僚友-苗】", "日【自己配偶-花】", "时【子孙-实】"))
+print("-"*150)
+print("{:^32s}{:^32s}{:^32s}{:^32s}".format(
+    gans.year + "--" + '{} [{}]'.format(gan5[gans.year], ten_deities[me][gans.year]),
+    gans.month + "--" + '{} [{}]'.format(gan5[gans.month], ten_deities[me][gans.month]),
+    me + "--" + '{} [{}]'.format(gan5[me], '自己'), 
+    gans.time + "--" + '{} [{}]'.format(gan5[gans.time], ten_deities[me][gans.time]),
 ))
-print("{:>11s}--{:<19s}{:>12s}--{:<19s}{:>12s}--{:<19s}{:>12s}--{:<19s}".format(
-    zhis.year, ten_deities[me][zhis.year],
-    zhis.month, ten_deities[me][zhis.month],
-    zhis.day, ten_deities[me][zhis.day],
-    zhis.time, ten_deities[me][zhis.time],
+print("{:^32s}{:^32s}{:^32s}{:^32s}".format(
+    zhis.year + "--" + ten_deities[me][zhis.year],
+    zhis.month + "--" + ten_deities[me][zhis.month],
+    zhis.day + "--" + ten_deities[me][zhis.day],
+    zhis.time + "--" + ten_deities[me][zhis.time],
 ))
 for item in zhis:
     out = ''
     for gan in zhi5[item]:
         out = out + "{}{}{}{} ".format(gan, gan5[gan], zhi5[item][gan],  ten_deities[me][gan])
-    print("{:<25s}".format(out), end=' ')
+    print("{:^28s}".format(out), end=' ')
 
 print("\n\n")
-print("="*140)  
-print("你属:", me, "特点：--", gan_atts[me])
+print("="*150)  
+print("你属:", me, "特点：--", gan_desc[me],"\n")
+print("年份:", zhis[0], "特点：--", zhi_desc[zhis[0]],"\n")
 print(dict(ten_deities[me]))
 #print(ten_deities[me])
 
@@ -115,7 +116,34 @@ check_subset(zhis, zhi_huis, '地支三会')
 check_subset(zhis, zhi_chongs, '地支相冲')	
 check_subset(zhis, zhi_poes, '地支相破')	
 check_subset(zhis, zhi_haies, '地支相害')	
-check_subset(zhis, zhi_xings, '地支相刑')	
+check_subset(zhis, zhi_xings, '地支相刑')
+
+# 子女分析
+child = ten_deities[me].inverse['伤'] if options.n else ten_deities[me].inverse['食']
+print("\n子女状态:", child, ten_deities[child][zhis[3]])  
+print("=========================")  
+
+# 对象状态
+peer = ten_deities[me].inverse['官'] if options.n else ten_deities[me].inverse['财']
+print("\n对象状态:", peer, ten_deities[peer][zhis[1]])  
+print("=========================")  
+
+# 羊刃分析
+key = '帝旺' if Gan.index(me)%2 == 0 else '冠带'
+
+if ten_deities[me].inverse[key] in zhis:
+    print("\n羊刃:", me, ten_deities[me].inverse[key])  
+    print("=========================")   
+    print("羊刃重重又见禄，富贵饶金玉。 官、印相助福相资。")  
+    print("专羊刃，主眼露性急，凶暴害物，亲近恶党，生旺稍可，死绝尤甚")
+    print("在五行败者逢之，多患瘰疠或瘴疠、金刃之灾，不论贵贱，多冗杂劳迫，少得安逸") 
+    print("六甲生人逢乙卯、丁卯，为真羊刃。若重犯，主残疾，官禄失退则散在晚年")
+    print("运行羊刃，财物耗散")    
+    print("\n有刃头财，如甲人见己卯之类，谓之销 煞 主财帛歇灭，常人以屠沽刀锯等事为业，或因被盗而致命者。")
+    print("有刃头鬼，如甲人见辛卯之类，谓之持刃煞，主人不令终，虽入贵格，亦不可测。甲乙人见之，尤紧。多脑疽发背而终。")
+    print("有羊刃相蚀，如甲寅虎、兔、甲戊狗兔之类()，见所蚀年月稍可，日时至危。若见两重，更值空亡，设非相蚀，亦犯流配至老，主不善终?")
+    print("有揽辔澄清格，谓贵人乘马而前视羊刃，犹马头带剑之义。")    
+    print("=========================")      
 
 
 guan_list = []
@@ -156,8 +184,9 @@ if len(guan_list) == 1:
         print("官冲",guan_list)    
 
     guan_xings = []
-    l = list(gans + zhis)
-    l.remove(gui)
+    l = list(zhis)
+    if gui in l:
+        l.remove(gui)
     for item in l:
         if item in xings[gui]:
             guan_xings.append(item)
@@ -186,7 +215,7 @@ if zhus[2] in tianyuans:
 scores = {"金":0, "木":0, "水":0, "火":0, "土":0}
 
 for item in gans:  
-    scores[gan5[item]] += 150
+    scores[gan5[item]] += 5
 
 for item in list(zhis) + [zhis.month]:  
     for gan in zhi5[item]:
